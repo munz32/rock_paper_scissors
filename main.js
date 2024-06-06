a13e651b478bf9aa78a1a5703479bb527a9c8cbf
Matt <matthew_mans@yahoo.com>
2024-06-05 21:53:54 -0500

    Updating final code for main.js

--- main.js
+++ main.js
@@ -3,27 +3,57 @@ let y;
 
 let humanScore = 0;
 let computerScore = 0;
+let counter =0;
 
 function getComputerChoice(){
     x = Math.ceil(Math.random()*3);
     if (x===1){
-        return("Rock");
+        return("rock");
     }  else if (x===2){
-        return("Paper")
+        return("paper")
     }  else{
-        return("Scissors")
+        return("scissors")
     }
 }
 
 
 function getHumanChoice(){
     y = prompt("What is your RPS selection?")
+    return(y)
 }
 
-function playRound(){
-    
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice()
+    computerChoice = getComputerChoice()
+
+    if (humanChoice === computerChoice){
+        console.log("It's a tie")
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else {
+        console.log("Computer wins");
+        computerScore++;
+    }    
 }
 
 
-console.log(getComputerChoice())
-getHumanChoice()
+function playGame(){
+    while (counter <5){
+        playRound();
+        counter++;
+    }
+    console.log(`The score is human: ${humanScore}, computer ${computerScore}`)
+}
+
+playGame()
